e90a3334cd2b51779646ed4abb123caa6197ae74
Bernardo Heynemann <heynemann@gmail.com>
2014-11-18 21:52:20 -0200

    CMake support

---
+++ src/server.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "JokerConfig.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!";
+	cout << " Version " << Joker_VERSION_MAJOR << "." << Joker_VERSION_MINOR << endl;
+}
